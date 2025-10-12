35855142f93f00c4549b4d2f1e13dde407016993
Innovation & Incubation Cell, BIET Jhansi <123385253+iicbietjhansi@users.noreply.github.com>
2025-10-12 12:15:27 +0530

    Update team.js

--- js/team.js
+++ js/team.js
@@ -104,3 +104,29 @@ var owl = $('#staff4');
       }
     }
 });
+var owl = $('#staff_heads');
+  owl.owlCarousel({
+    margin:20,
+    dots:true,
+    nav: true,
+    navText: [
+      "<i class='fa fa-chevron-left'></i>",
+      "<i class='fa fa-chevron-right'></i>"
+    ],
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items:2
+      },
+      1200: {
+        items:4
+      }
+    }
+});
